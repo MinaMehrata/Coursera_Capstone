8b5fe8175bca2cffee2db1f2a5d51d2ad1d86505
Mina <m.mehrata@gmail.com>
2020-07-31 10:23:33 -0400

    Create Ipython.py

---
+++ Ipython.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# # The Capstone Project
+# 
+# this notenbook will use for the capstone project.
+
+# In[1]:
+
+
+import numpy
+import pandas
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
